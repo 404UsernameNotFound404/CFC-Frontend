fcc895eadaf38794379b929bd4847fda858785b1
Henry Morris <2001henrymorris@gmail.com>
2020-07-13 23:47:42 -0400

    dooxtoons suguestions

--- src/Components/Pages/FAQ/FAQPage.tsx
+++ src/Components/Pages/FAQ/FAQPage.tsx
@@ -1,40 +1,61 @@
-import React from 'react'
-import styled from 'styled-components'
-import Question from './Question'
+import React from "react";
+import styled from "styled-components";
+import Question from "./Question";
 
 const Page = styled.div`
-    padding-top: 5em;
+  padding-top: 5em;
+  margin: auto;
+  margin-bottom: 3em;
+  width: 65em;
+  @media (max-width: ${process.env.REACT_APP_PHONE_BREAK}px) {
+    width: 90%;
     margin: auto;
-    margin-bottom: 3em;
-    width: 65em;
-    @media (max-width: ${process.env.REACT_APP_PHONE_BREAK}px) {
-        width: 90%;
-        margin: auto;
-    }
+  }
 `;
 
 const PageTitle = styled.h1`
-    font-family: 'Scope One', serif;
-    font-size: 4em;
-    margin: 0;
+  font-family: "Scope One", serif;
+  font-size: 4em;
+  margin: 0;
 `;
 
 function FAQPage() {
-    const faqs = [
-        { question: "Do I have to already be an activist?", answer: "No, everyone has to start somewhere. We want to help you get involved in activism." },
-        { question: "Is Connecting For Change a grassroots organization?", answer: "Yes." },
-        { question: "How can I submit feedback?", answer: "Please email us. Our email is admin@connecting-for-change.ca" },
-        { question: "Is Connecting For Change affiliated with a political party?", answer: "No. Connecting For Change is about social/environmental justice and human rights." },
-        { question: "How can I get involved with Connecting For Change?", answer: "Please reach out to us at: admin@connecting-for-change.ca" }
-    ]
-    return (
-        <Page>
-            <PageTitle>Frequently Asked Questions</PageTitle>
-            {
-                faqs.map((ele, i) => <Question question={ele.question} answer={ele.answer} key={i} />)
-            }
-        </Page>
-    )
+  const faqs = [
+    {
+      question: "Do I have to already be an activist?",
+      answer:
+        "No, everyone has to start somewhere. We want to help you get involved in activism.",
+    },
+    {
+      question: "Is Connecting For Change a grassroots organization?",
+      answer: "Yes.",
+    },
+    {
+      question: "How can I submit feedback?",
+      answer: "Please email us. Our email is admin@connecting-for-change.ca",
+    },
+    {
+      question: "Is Connecting For Change affiliated with a political party?",
+      answer:
+        "No. Connecting For Change is about social/environmental justice and human rights.",
+    },
+    {
+      question: "How can I get involved with Connecting For Change?",
+      answer: "Please reach out to us at: admin@connecting-for-change.ca",
+    },
+    {
+      question: "My question is not here, what should I do?",
+      answer: "Please reach out to us at: admin@connecting-for-change.ca",
+    },
+  ];
+  return (
+    <Page>
+      <PageTitle>Frequently Asked Questions</PageTitle>
+      {faqs.map((ele, i) => (
+        <Question question={ele.question} answer={ele.answer} key={i} />
+      ))}
+    </Page>
+  );
 }
 
 export default FAQPage;
--- src/Components/Pages/Search/Organizations.tsx
+++ src/Components/Pages/Search/Organizations.tsx
@@ -1,140 +1,185 @@
-import React, { useState, useEffect, useContext } from 'react';
-import styled from 'styled-components';
-import { getSearchData } from '../../packages/search-page-functions/getSearchData';
-import { AppContext } from '../../../Context/AppContext';
-import OrganizationCard, { ActionButtonProps } from '../../packages/organization-card-react/organizationsCard';
-import { checkIfInCategories } from '../../packages/search-page-functions/checkIfHasCategories';
-import Modal from '../../packages/modal-react';
-import CreatingEditingOrg from '../../packages/organization-card-react/creatingEditingOrgs';
-import ReactLoading from 'react-loading';
+import React, { useState, useEffect, useContext } from "react";
+import styled from "styled-components";
+import { getSearchData } from "../../packages/search-page-functions/getSearchData";
+import { AppContext } from "../../../Context/AppContext";
+import OrganizationCard, {
+  ActionButtonProps,
+} from "../../packages/organization-card-react/organizationsCard";
+import { checkIfInCategories } from "../../packages/search-page-functions/checkIfHasCategories";
+import Modal from "../../packages/modal-react";
+import CreatingEditingOrg from "../../packages/organization-card-react/creatingEditingOrgs";
+import ReactLoading from "react-loading";
 
 const Component = styled.div`
-    width: 100%;
+  width: 100%;
 `;
 
 const Para = styled.p`
-    font-size: 1.5rem;
-    text-align: center;
+  font-size: 1.5rem;
+  text-align: center;
 `;
 
 const CreateOrgButton = styled.div`
-    width: fit-content;
-    border-radius: 0.5em;
-    padding: 0.5em 1em;
-    background-color: #3c78d8;
-    margin-bottom: 1em;
-    color: white;
-    cursor: pointer;
-    &:hover {
-        background-color: #183e7c;
-    }
+  width: fit-content;
+  border-radius: 0.5em;
+  padding: 0.5em 1em;
+  background-color: #3c78d8;
+  margin-bottom: 1em;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #183e7c;
+  }
 `;
 
 const OrganizationContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    @media (max-width: ${process.env.REACT_APP_PHONE_BREAK}px) {
-        width: 100% !important;
-        display: inline-block;
-    }
-    margin-bottom: 5em;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  @media (max-width: ${process.env.REACT_APP_PHONE_BREAK}px) {
+    width: 100% !important;
+    display: inline-block;
+  }
+  margin-bottom: 5em;
 `;
 
 type Props = {
-    categoriesToShow: number[];
-}
+  categoriesToShow: number[];
+};
 
 const RequestModalButtonStyle = styled.div`
-    width: fit-content;
-    padding: 0.5em 0.75em;
-    color: #3c78d8;
-    border-radius: 0.3em;
-    /* background-color: #3c78d8; */
-    border: thin solid #3c78d8;
-    cursor: pointer;
-    font-size: 1em;
-    &:hover {
-        color: #183e7c;
-        background-color: #3c78d81f;
-        border-color: #183e7c;
-    }
+  width: fit-content;
+  padding: 0.5em 0.75em;
+  color: #3c78d8;
+  border-radius: 0.3em;
+  /* background-color: #3c78d8; */
+  border: thin solid #3c78d8;
+  cursor: pointer;
+  font-size: 1em;
+  &:hover {
+    color: #183e7c;
+    background-color: #3c78d81f;
+    border-color: #183e7c;
+  }
 `;
 
 const LoadingContainer = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
 
 const RequestModalButton = (props: ActionButtonProps) => {
-    return (
-        <RequestModalButtonStyle onClick={() => { props.ActionButtonOnClick(props.id) }}>Request Change</RequestModalButtonStyle>
-    );
-}
+  return (
+    <RequestModalButtonStyle
+      onClick={() => {
+        props.ActionButtonOnClick(props.id);
+      }}
+    >
+      Request Change
+    </RequestModalButtonStyle>
+  );
+};
 
-const sortArrayAlphabet = (data: Array<{ _id: object, data: { name: string } }> | Array<{ Name: string }>) => {
-    data = data.sort(function (a: any, b: any) {
-        const textA = a.name.toUpperCase().trim();
-        const textB = b.name.toUpperCase().trim();
-        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-    });
-    return data;
-}
+const sortArrayAlphabet = (
+  data: Array<{ _id: object; data: { name: string } }> | Array<{ Name: string }>
+) => {
+  data = data.sort(function (a: any, b: any) {
+    const textA = a.name.toUpperCase().trim();
+    const textB = b.name.toUpperCase().trim();
+    return textA < textB ? -1 : textA > textB ? 1 : 0;
+  });
+  return data;
+};
 
 function Organizations(props: Props) {
-    const [organizations, setOrganizations] = useState([]);
-    const [modalData, setModalData] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [organizations, setOrganizations] = useState([]);
+  const [modalData, setModalData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getOrgs();
-    }, []);
+  useEffect(() => {
+    getOrgs();
+  }, []);
 
-    const getOrgs = async () => {
-        setLoading(true);
-        const orgsData = await getSearchData("Organizations");
-        if (orgsData.error != undefined) console.log("error");//TODO
-        else {
-            setOrganizations(sortArrayAlphabet(orgsData));
+  const getOrgs = async () => {
+    setLoading(true);
+    const orgsData = await getSearchData("Organizations");
+    if (orgsData.error != undefined) console.log("error");
+    //TODO
+    else {
+      setOrganizations(sortArrayAlphabet(orgsData));
+    }
+    setLoading(false);
+  };
+
+  const openModal = (orgId: string) =>
+    setModalData(organizations.find((ele) => ele._id == orgId));
+
+  const createModal = () => setModalData({ create: true });
+
+  const closeModal = () => setModalData(null);
+
+  const closeModalAndRefresh = () => {
+    console.log("hello this should be working");
+    closeModal();
+    getOrgs();
+  };
+
+  if (organizations.length != 0 && !loading) {
+    return (
+      <Component>
+        <Para>
+          We do not yet have contact with all the organizations on our list. We
+          have compiled this list to help activists find organizations. If you
+          do not see an organization on our list, please add it.{" "}
+        </Para>
+        <CreateOrgButton onClick={createModal}>
+          Enter An Organization
+        </CreateOrgButton>
+        <OrganizationContainer>
+          {Array.isArray(organizations) &&
+            organizations.map(
+              (ele, i) =>
+                checkIfInCategories(ele.interests, props.categoriesToShow) && (
+                  <OrganizationCard
+                    key={i}
+                    ActionButton={RequestModalButton}
+                    ActionButtonOnClick={openModal}
+                    {...ele}
+                  />
+                )
+            )}
+        </OrganizationContainer>
+        <CreateOrgButton onClick={createModal}>
+          Enter An Organization
+        </CreateOrgButton>
+        {
+          <Modal close={!modalData} setClose={closeModal}>
+            {modalData && (
+              <CreatingEditingOrg
+                setLoading={setLoading}
+                closeModal={closeModalAndRefresh}
+                edit={modalData.create == undefined}
+                _id={modalData != null ? modalData._id : ""}
+                {...modalData}
+              />
+            )}
+          </Modal>
         }
-        setLoading(false);
-    }
-
-    const openModal = (orgId: string) => setModalData(organizations.find(ele => ele._id == orgId));
-
-    const createModal = () => setModalData({ create: true });
-
-    const closeModal = () => setModalData(null);
-
-    const closeModalAndRefresh = () => {
-        console.log("hello this should be working")
-        closeModal();
-        getOrgs();
-    }
-
-    if (organizations.length != 0 && !loading) {
-        return (
-            <Component>
-                <Para>We do not yet have contacts with all the organizations on our list. We have compiled this list to help activists find organizations. If you do not see an organization on our list, please add it. </Para>
-                <CreateOrgButton onClick={createModal}>Enter An Organization</CreateOrgButton>
-                <OrganizationContainer>
-                    {
-                        Array.isArray(organizations) && organizations.map((ele, i) => checkIfInCategories(ele.interests, props.categoriesToShow) && <OrganizationCard key={i} ActionButton={RequestModalButton} ActionButtonOnClick={openModal} {...ele} />)
-                    }
-                </OrganizationContainer>
-                <CreateOrgButton onClick={createModal}>Enter An Organization</CreateOrgButton>
-                {
-                    <Modal close={!modalData} setClose={closeModal}>
-                        {modalData && <CreatingEditingOrg setLoading={setLoading} closeModal={closeModalAndRefresh} edit={modalData.create == undefined} _id={modalData != null ? modalData._id : ""} {...modalData} />}
-                    </Modal>
-                }
-
-            </Component>
-        );
-    } else {
-        return <LoadingContainer><ReactLoading type={"bubbles"} color={"blue"} height={"10%"} width={'25%'} /></LoadingContainer>;
-    }
+      </Component>
+    );
+  } else {
+    return (
+      <LoadingContainer>
+        <ReactLoading
+          type={"bubbles"}
+          color={"blue"}
+          height={"10%"}
+          width={"25%"}
+        />
+      </LoadingContainer>
+    );
+  }
 }
 
 export default Organizations;

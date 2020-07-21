3c2326d961cc6419c18f2b0999d77dc456610bce
Henry Morris <2001henrymorris@gmail.com>
2020-07-20 20:57:29 -0400

    Fixed Category bug

--- src/Components/packages/search-page-functions/checkIfHasCategories.ts
+++ src/Components/packages/search-page-functions/checkIfHasCategories.ts
@@ -1,7 +1,23 @@
-export const checkIfInCategories = (arrayToCheck: number[], categoriesToAllow: number[]) => {
-    let render = false;
-    for (let x = 0; x < categoriesToAllow.length; x++) {
-        if (arrayToCheck.find((catEle: number) => categoriesToAllow[x] === catEle) != null) return true
+export const checkIfInCategories = (
+  arrayToCheck: number[],
+  categoriesToAllow: number[]
+) => {
+  let render = false;
+  for (let x = 0; x < categoriesToAllow.length; x++) {
+    if (typeof categoriesToAllow == "number") {
+      if (
+        arrayToCheck.find(
+          (catEle: number) => categoriesToAllow[x] === catEle
+        ) != null
+      )
+        return true;
+    } else {
+      if (
+        !!arrayToCheck.find((catEle: any) => catEle.ID == categoriesToAllow[x])
+      ) {
+        return true;
+      }
     }
-    return (render || categoriesToAllow.length <= 0)
-}
+  }
+  return render || categoriesToAllow.length <= 0;
+};
